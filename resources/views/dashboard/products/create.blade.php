e9fef4bc3aab99e54a9e68635d4ef1c6a724fc85
EC2 Default User <ec2-user@ip-172-31-27-168.ap-northeast-1.compute.internal>
2021-02-18 08:25:02 +0000

    create products create page

---
+++ resources/views/dashboard/products/create.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.dashboard')
+
+@section('content')
+
+<div class="dashboard-container">
+    <h1>商品登録</h1>
+    
+    <form method="POST" action="/dashboard/products">
+        {{ csrf_field() }}
+        <div class="form-inline row">
+            <label for="product_name">商品名</label>
+            <input type="text" name="name" id="product_name">
+        </div>
+        <div class="form-inline row">
+            <label for="width">巾</label>
+            <input type="text" name="width" id="width">
+        </div>
+        <div class="form-inline row">
+            <label for="moq">1に対する長さ</label>
+            <input type="text" name="moq" id="moq">
+        </div>
+        <div class="form-inline row">
+            <label for="description">説明</label>
+            <input type="text" name="description" id="description">
+        </div>
+        <div class="form-inline row">
+            <label for="price">価格</label>
+            <input type="text" name="price" id="price">
+        </div>
+        <div class="form-inline row">
+            <label for="stock">在庫</label>
+            <input type="text" name="stock" id="stock">
+        </div>
+        <div class="form-inline row">
+            <label for="special_feature">適用する特集</label>
+            <input type="text" name="special_feature" id="special_feature">
+        </div>
+        <div class="form-inline row">
+            <label for="restock">適用する特集</label>
+            <input type="date" name="restock" id="restock">
+        </div>
+        <div class="d-flex justify-content-end">
+            <button type="submit" class="w-25 btn samazon-submit-button">商品を登録</button>
+        </div>
+    </form>
+</div>
+
+@endsection
+
