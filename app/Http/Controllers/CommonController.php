6599c6ab2c80fdd20c4962f973cd96d92514ca02
v531aki <v531aki@gmail.com>
2021-03-09 22:27:24 +0000

    カレンダーの追加

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Ranking;
 use App\Category;
+use Carbon\Carbon;
 use App\Product;
 
 use Gloudemans\Shoppingcart\Facades\Cart;
@@ -32,6 +33,25 @@ class CommonController
             }
         }
 
+        $common['dates'] = $this->getCalendarDates(date('Y'),date('m'));
+
         return $common;
+    }
+    public function getCalendarDates($year, $month)
+    {
+        $dateStr = sprintf('%04d-%02d-01', $year, $month);
+        $date = new Carbon($dateStr);
+        // カレンダーを四角形にするため、前月となる左上の隙間用のデータを入れるためずらす
+        $date->subDay($date->dayOfWeek);
+        // 同上。右下の隙間のための計算。
+        $count = 31 + $date->dayOfWeek;
+        $count = ceil($count / 7) * 7;
+        $dates = [];
+
+        for ($i = 0; $i < $count; $i++, $date->addDay()) {
+            // copyしないと全部同じオブジェクトを入れてしまうことになる
+            $dates[] = $date->copy();
+        }
+        return $dates;
     }
 }
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -14,3 +14,55 @@
     </div>
     @endforeach
 </div>
+<div class="container px-0 mt-5">
+    <h2 class="text-center">{{ $sidebar['dates'][8]->month }}月</h2>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th class="text-center text-danger px-0 py-2">日</th>
+                @foreach (['月', '火', '水', '木', '金'] as $dayOfWeek)
+                <th class="text-center px-0 py-2">{{ $dayOfWeek }}</th>
+                @endforeach
+                <th class="text-center text-primary px-0 py-2">土</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($sidebar['dates'] as $date)
+        @if ($date->dayOfWeek == 0)
+        <tr>
+            <td
+            @if ($date->month != date('m'))
+            class="px-0 py-2 bg-secondary text-center"
+            @else
+            class="px-0 py-2 bg-primary text-white text-center"
+            @endif
+            >
+                {{ $date->day }}
+            </td>
+        @elseif ($date->dayOfWeek == 6)
+            <td
+            @if ($date->month != date('m'))
+            class="px-0 py-2 bg-secondary text-center"
+            @else
+            class="px-0 py-2 bg-primary text-white text-center"
+            @endif
+            >
+                {{ $date->day }}
+            </td>
+        </tr>
+        @else
+            <td
+            @if ($date->month != date('m'))
+            class="px-0 py-2 bg-secondary text-center"
+            @else
+            class="px-0 py-2 text-center"
+            @endif
+            >
+                {{ $date->day }}
+            </td>
+        @endif
+
+        @endforeach
+        </tbody>
+    </table>
+</div>
